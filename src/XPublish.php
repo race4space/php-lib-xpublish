42720f6696ec190c38c7e2eddc3879de220817e6
race4space <58293087+race4space@users.noreply.github.com>
2020-04-08 12:05:38 +0200

    xxx

--- src/XPublish.php
+++ src/XPublish.php
@@ -35,14 +35,15 @@ echo <<< CLIENT
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>  
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
     <script src="/data-xplorer/_client/data.js"></script>
     <script src="/data-xplorer/_client/modal.js"></script>
 
-    <link rel="stylesheet" type="text/css" href="/theme/mystyle_v3.css">
+    
+    <link rel="stylesheet" type="text/css" href="/xpublish/client/mystyle_v3.css">
     <title>Console</title>
     <body style="overflow-y: scroll;">
 CLIENT;
