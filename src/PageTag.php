7613e5ea931bb8a3bc50af1334b49f4cf91eef15
race4space <58293087+race4space@users.noreply.github.com>
2020-04-08 14:21:00 +0200

    xxx

--- src/PageTag.php
+++ src/PageTag.php
@@ -4,16 +4,24 @@ class PageTag extends Page {
   function __construct() {
 
     parent::__construct("mytagOpen");
-    $this->str_type="pageData";
+    $this->str_type="pageTag";
   }
   function fn_execute() {
     try{
       $this->fn_write_containerBox_start();
       echo '<'.$this->str_tag.'>'.PHP_EOL;
       echo '</'.$this->str_tag.'>'.PHP_EOL;
+      //DEFAULT SCRIPTS SUPPLIED HERE
+      //TO DO : Do LOOK UP IN DB TO DETEMRINE WHICH SCRIPTS THE TAG NEEDS
+      echo '<script src="/xpublish/_client/data.js"></script>'.PHP_EOL;
+      echo '<script src="/xpublish/_client/modal.js"></script>'.PHP_EOL;
+      /*
       echo '<script src="/'.$this->str_tag.'/_client/data.js"></script>'.PHP_EOL;
       echo '<script src="/'.$this->str_tag.'/_client/modal.js"></script>'.PHP_EOL;
-      echo '<script src="/'.$this->str_tag.'/_client/tag.js"></script>'.PHP_EOL;
+      //*/
+      //echo '<script src="/'.$this->str_tag.'/_client/tag.js"></script>'.PHP_EOL;
+
+      echo '<script src="/xpublish/app/'.$this->str_tag.'/_client/tag.js"></script>'.PHP_EOL;
       $this->fn_write_containerBox_end();
     }
     catch(PDOException $e) {
