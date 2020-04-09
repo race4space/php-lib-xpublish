892331783e1acb1d629b63c36c51c123901681a5
race4space <58293087+race4space@users.noreply.github.com>
2020-04-09 14:30:01 +0200

    xxx

--- src/PageTag.php
+++ src/PageTag.php
@@ -13,13 +13,16 @@ class PageTag extends Page {
       echo '</'.$this->str_tag.'>'.PHP_EOL;
       //DEFAULT SCRIPTS SUPPLIED HERE
       //TO DO : Do LOOK UP IN DB TO DETEMRINE WHICH SCRIPTS THE TAG NEEDS
+      /*
       echo '<script src="/xpublish/_client/data.js"></script>'.PHP_EOL;
       echo '<script src="/xpublish/_client/modal.js"></script>'.PHP_EOL;
-      /*
+      echo '<script src="/'.$this->str_tag.'/_client/tag.js"></script>'.PHP_EOL;
+      //*/
+      //*
       echo '<script src="/'.$this->str_tag.'/_client/data.js"></script>'.PHP_EOL;
       echo '<script src="/'.$this->str_tag.'/_client/modal.js"></script>'.PHP_EOL;
-      //*/
       echo '<script src="/'.$this->str_tag.'/_client/tag.js"></script>'.PHP_EOL;
+      //*/      
 
       //echo '<script src="/xpublish-app/'.$this->str_tag.'/_client/tag.js"></script>'.PHP_EOL;
       $this->fn_write_containerBox_end();
